ecfa9e5bd4f43b2d980d9857dbd08334f276171b
BanurekhaV <rekhasairam@hotmail.com>
2022-02-02 18:34:31 +0530

    Feb 2nd

---
+++ Day12/Feb2Prj/Feb2Prj/Features.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Convert;
+using static System.Console;
+using System.Threading;
+
+namespace Feb2Prj
+{
+
+    class Features
+    {
+        public static int year = 2017;
+        static void Main()
+        {
+            try
+            {
+                int[] a = new int[5];
+                a[1] = 20;
+                int number = 0;
+                int answer = 5 / number;
+            }
+            catch(Exception e) when (e.GetType().ToString()=="System.IndexOutOfRangeException")
+            {
+                SomeotherFunction();
+            }
+            catch(DivideByZeroException) when (DateTime.Now.DayOfWeek!=DayOfWeek.Monday)
+            {
+                Console.WriteLine("Sorry, Today is not Monday");
+            }
+            catch (DivideByZeroException) when (DateTime.Now.DayOfWeek == DayOfWeek.Wednesday)
+            {
+                Console.WriteLine("Welcome.. You can register today");
+            }
+            //literal improvements (_)
+            var lit1 = 476_487_586_98; //digit seperators
+            var lit2 = 0xa3_7e;   //hexadecimal literal
+            var lit3 = 1100_1011_0100_1001;
+            Console.WriteLine($"lit1 :{lit1}, lit2: {lit2}, lit3: {lit3}");
+            Console.WriteLine(LeapYear());
+            Console.WriteLine("enter  the side of square :");
+            int s = ToInt32(ReadLine());
+            int square = AreaofSquare(s);
+            Thread.Sleep(2000);
+            Write($"Area of Square is {square}");
+            Console.Read();
+        }
+        static void SomeotherFunction()
+        {
+            Console.WriteLine("This is a new Task...");
+        }
+
+        //normal way of writing a function
+        //public static string LeapYear()
+        //{
+        //    return "\n Is" + year + " a leap Year -" + DateTime.IsLeapYear(year);
+        //}
+        //with expression bodied member
+        public static string LeapYear() => $"\n  Is {year}  a leap year - " + DateTime.IsLeapYear(year);
+        public static int AreaofSquare(int side) => side * side;
+    }
+}
---
+++ Day12/Feb2Prj/Feb2Prj/Initializers.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Feb2Prj
+{
+    class Student
+    {
+       public int StudentId;
+      public string StudntName;
+    }
+    class Initializers
+    {
+        static void Main()
+        {
+            List<Student> studlist = new List<Student>()
+            {
+                new Student(){StudentId=1,StudntName="Rajesh"},
+                new Student(){StudentId=2,StudntName="Rakesh"},
+                new Student(){StudentId=3,StudntName="Sukesh"},
+                null
+            };
+            var div = Divide(10, 2);
+            IEnumerable<string> names = from s in studlist
+                                        where s.StudntName.StartsWith("R")
+                                        select s.StudntName;
+
+            foreach(var std in names)
+            {
+                Console.WriteLine(std);
+            }
+            Console.Read();
+        }
+        public static double Divide(int x, int y)
+        {
+            return y != 0 ? x / y : throw new DivideByZeroException();
+        }
+    }
+}
