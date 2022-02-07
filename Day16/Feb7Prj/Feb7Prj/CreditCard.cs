c2059c2a4cd5350e963aa4f7044a5911d0181667
BanurekhaV <rekhasairam@hotmail.com>
2022-02-07 10:57:25 +0530

    FactoryPattern

---
+++ Day16/Feb7Prj/Feb7Prj/CreditCard.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Feb7Prj
+{
+    //before implementation of Factory Pattern
+    public interface CreditCard
+    {
+        string GetCardType();
+        int GetCreditLimit();
+        int GetAnnualCharges();
+    }
+   
+}
