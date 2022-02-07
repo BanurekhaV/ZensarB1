bde10e67f8aeca72e571974cdc12635a74345d7c
BanurekhaV <rekhasairam@hotmail.com>
2022-02-07 15:57:38 +0530

    Abstract factory and Singleton added

---
+++ Day16/AbstractFactoryPattern/AbstractFactoryPattern/ConcreteAnimals.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactoryPattern
+{
+    public class Cat : Animal
+    {
+        public string speak()
+        {
+            return "Meow Meow";
+        }
+    }
+
+    public class Dog : Animal
+    {
+        public string speak()
+        {
+            return "Bow Bow";
+        }
+    }
+
+    public class Lion : Animal
+    {
+        public string speak()
+        {
+            return "Roar";
+        }
+    }
+    public class Octopus : Animal
+    {
+        public string speak()
+        {
+            return "Squawck";
+        }
+    }
+
+    public class Shark : Animal
+    {
+        public string speak()
+        {
+            return "I cannot Speak";
+        }
+    }
+    
+}
