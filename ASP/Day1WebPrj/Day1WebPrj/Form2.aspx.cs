dd8c0037c59f9630dfb4e9fb3d22ece1ca0fdaa8
BanurekhaV <rekhasairam@hotmail.com>
2022-03-02 18:09:47 +0530

    asp

---
+++ ASP/Day1WebPrj/Day1WebPrj/Form2.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Day1WebPrj
+{
+    public partial class Form2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Response.Write("Page load Successful." + "<br/>");
+        }
+
+        protected void Page_initComplete(object sender, EventArgs e)
+        {
+            Response.Write("Page  Init  complete Successfull.." + "<br/>");
+        }
+        protected void Page_Init(object sender, EventArgs e)
+        {
+            Response.Write("Page Init Successfull.." + "<br/>");
+        }
+        protected void Page_PreInit(object sender, EventArgs e)
+        {
+            Response.Write("Page Pre Init Successfull.." + "<br/>");
+        }
+
+        protected void Page_PreLoad(object sender, EventArgs e)
+        {
+            Response.Write("Page Pre Load Successfull.." + "<br/>");
+        }
+        protected void Page_PreRender(object sender, EventArgs e)
+        {
+            Response.Write("Page Pre Render  Successfull.." + "<br/>");
+        }
+        protected void Page_Unload(object sender, EventArgs e)
+        {
+          //  Response.Write("Page unloading Successfull..");
+        }
+        protected void Page_Render(object sender, EventArgs e)
+        {
+            Response.Write("Page Render Successfull.." + "<br/>");
+        }
+
+        protected void Page_PreRenderComplete(object sender, EventArgs e)
+        {
+            Response.Write("Page Render Complete Successfull.." + "<br/>");
+        }
+    }
+}
---
+++ ASP/Day1WebPrj/Day1WebPrj/IntroForm.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="IntroForm.aspx.cs" Inherits="Day1WebPrj.IntroForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 118px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server">Enter Name:</asp:Label>&nbsp;&nbsp;
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+           
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click1" Text="Button" />
+           
+            </div>
+    </form>
+</body>
+</html>
+&nbsp;
