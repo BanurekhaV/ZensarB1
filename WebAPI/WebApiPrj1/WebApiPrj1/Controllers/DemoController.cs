ba3294de9900b3839914de7e2ae00bfd7b16a39d
BanurekhaV <rekhasairam@hotmail.com>
2022-03-14 18:07:11 +0530

    api

---
+++ WebAPI/WebApiPrj1/WebApiPrj1/Controllers/DemoController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApiPrj1.Models;
+
+namespace WebApiPrj1.Controllers
+{
+    public class DemoController : ApiController
+    {
+        static List<string> strings = new List<string> { "value0", "value1", "value2", "value3" };
+
+        //GET : api/Demo/
+        public IEnumerable<string> Get()
+        {
+            return strings;
+        }
+
+        //GET: api/Demo/2
+        public string Get(int id)
+        {
+            return strings[id];
+        }
+
+        //POST type 1
+
+        //public IEnumerable<string> Post([FromBody] string val)
+        //{
+        //    strings.Add(val);
+        //    return strings;
+        //}
+        [HttpPost]
+        //Post Type 2 with a model
+        public IEnumerable<string> Addstrings([FromBody] SampleModel m)
+        {
+            strings.Add(m.name);
+            return strings;
+        }
+
+        //PUT: api/Demo/id
+
+        public IEnumerable<string> Put(int id, [FromUri] string val)
+        {
+            strings[id-1] = val;
+            return strings;
+        }
+
+        //DELETE : api/Demo/id
+
+        public IEnumerable<string> Delete(int id)
+        {
+            strings.RemoveAt(id);
+            return strings;
+        }
+    }
+}
---
+++ WebAPI/WebApiPrj1/WebApiPrj1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApiPrj1.WebApiApplication" Language="C#" %>
