f4f8b8809cdc887c50b2857c8295bef97cc0e68f
BanurekhaV <rekhasairam@hotmail.com>
2022-03-24 12:06:56 +0530

    Model Validations

--- Core/Core3_Taghelpers/Core3_Taghelpers/Controllers/JobController.cs
+++ Core/Core3_Taghelpers/Core3_Taghelpers/Controllers/JobController.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Core3_Taghelpers.Models;
 
 namespace Core3_Taghelpers.Controllers
 {
@@ -11,6 +12,30 @@ namespace Core3_Taghelpers.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(JobApplication JA)
+        {
+            //if (string.IsNullOrEmpty(JA.Name))
+            //    ModelState.AddModelError(nameof(JA.Name), "Please enter your Name");
+            //else if (JA.Name == "Banu Rekha")
+            //    ModelState.AddModelError("", "You are not Eligible to apply for the job");
+            if (JA.DOB == Convert.ToDateTime("01-01-0001 00:00:00"))
+                ModelState.AddModelError(nameof(JA.DOB), "Please enter Your Date of Birth");
+            else if (JA.DOB > DateTime.Now)
+                ModelState.AddModelError(nameof(JA.DOB), "Date of Birth cannot be future date");
+            else if (JA.DOB < new DateTime(1980, 1, 1))
+                ModelState.AddModelError(nameof(JA.DOB), "Date of Birth cannot be before 1980");
+            //if (JA.Experience.ToString() == "Select")
+            //    ModelState.AddModelError(nameof(JA.Experience), "Please select your experience");
+            //if (!JA.AcceptTerms)
+            //    ModelState.AddModelError(nameof(JA.AcceptTerms), "You must accept the Terms");
+            //Error applicable for the entire model and not for a particular property
+           // ModelState.AddModelError("", "Some Model Level Error Occurred");
+            if(ModelState.IsValid)
+                 return View("Accepted", JA);
+            else return View();
         }
     }
 }
--- Core/Core3_Taghelpers/Core3_Taghelpers/Models/JobApplication.cs
+++ Core/Core3_Taghelpers/Core3_Taghelpers/Models/JobApplication.cs
@@ -2,14 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace Core3_Taghelpers.Models
 {
     public class JobApplication
     {
+        [Required(ErrorMessage ="Name cannot be Blank")]
+        [Display(Name="Job Applicant's Name:")]
         public string Name { get; set; }
         public DateTime DOB { get; set; }
+        [Range(0,5)]
         public string Experience { get; set; }
+        [Range(typeof(bool),"true","true",ErrorMessage ="You must accept the Terms")]
         public bool AcceptTerms { get; set; }
     }
 }
